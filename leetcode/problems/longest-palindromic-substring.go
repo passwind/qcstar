a6b5eb348e03532d68c5eb354b522fd8295b68d5
Zhu Yu <mike.zhuyu@gmail.com>
2018-07-13 09:54:35 +0800

    Update longest-palindromic-substring

--- leetcode/problems/longest-palindromic-substring.go
+++ leetcode/problems/longest-palindromic-substring.go
@@ -1,44 +1,82 @@
 package problems
 
-import "fmt"
-
 func longestPalindrome(s string) string {
-	fmt.Printf("string is %s\n", s)
+	total := len(s)
 	if len(s) <= 1 {
 		return s
 	}
 
-	l, r, m, ml, mr := 0, 0, 1, 0, 0
-	var t byte
-	for idx, c := range s {
-		fmt.Printf("l, r, m, ml, mr, idx = %d, %d, %d, %d, %d, %d\n", l, r, m, ml, mr, idx)
-		b := byte(c)
-		if b == t {
-			r = idx
-			m++
-		} else {
-			t = b
-			if idx-(2*m-1) >= 0 && b == s[idx-(2*m-1)] { // 无中点对称, 如bbbb，aa
-				l = idx - (2*m - 1)
-				r = idx
-				m++
-			} else if idx-2*m >= 0 && b == s[idx-2*m] { // 有中点对称，如bbb，aba
-				l = idx - 2*m
-				r = idx
-				m++
-			} else {
-				m = 1
-				l = idx
+	checkMax := func(ml int, idx int) bool {
+		l := idx - ml/2
+		r := idx + ml/2
+		if ml%2 == 0 {
+			r = idx + ml/2 - 1
+		}
+		i, j := l, r
+		if l < 0 || r >= total {
+			return false
+		}
+		for {
+			if i >= j {
+				break
 			}
+			if s[i] != s[j] {
+				return false
+			}
+			i++
+			j--
 		}
 
-		if mr-ml < r-l {
-			ml = l
-			mr = r
+		return true
+	}
+
+	maxlen := 1
+	maxstr := s[0:1]
+
+	for idx := 1; idx < total; idx++ {
+		substr, ok := func() (string, bool) {
+			ml1, ml2 := maxlen+1, maxlen+2
+			ml1ok := checkMax(ml1, idx)
+			ml2ok := checkMax(ml2, idx)
+			var ml int
+			if ml1ok && ml2ok {
+				if ml1 > ml2 {
+					ml = ml1
+				} else {
+					ml = ml2
+				}
+			} else if ml1ok {
+				ml = ml1
+			} else if ml2ok {
+				ml = ml2
+			} else {
+				return "", false
+			}
+			var l, r int
+			if ml%2 == 0 {
+				l = idx - ml/2
+				r = idx + ml/2 - 1
+			} else {
+				l = idx - ml/2
+				r = idx + ml/2
+			}
+
+			i := l - 1
+			j := r + 1
+			for {
+				if i < 0 || j == total || s[i] != s[j] {
+					break
+				}
+				i--
+				j++
+			}
+			return s[i+1 : j], true
+		}()
+		if ok {
+			maxlen = len(substr)
+			maxstr = substr
 		}
 	}
 
-	fmt.Printf("ml, mr = %d, %d\n", ml, mr)
-
-	return s[ml : mr+1]
+	return maxstr
 }
--- leetcode/problems/longest-palindromic-substring.md
+++ leetcode/problems/longest-palindromic-substring.md
@@ -1,1 +1,37 @@
 # [最长回文子串](https://leetcode-cn.com/problems/longest-palindromic-substring/description/)
+
+***执行用时4ms***
+
+![提交记录](./longest-palindromic-substring.png)
+
+***执行用时为 0 ms 的范例***
+
+```golang
+func longestPalindrome(s string) string {
+	str := []byte(s)
+	if len(s) < 2 {
+		return s
+	}
+	index := 0
+	maxLen,start := 1,0
+	for index < len(s) {
+		if maxLen/2 >= len(s) - index{
+			break
+		}
+		j,k := index,index
+		for k < len(s) - 1 && str[k+1] == str[k] {
+			k++
+		}
+		index = k+1
+		for k < len(s)-1 && j > 0 && str[j-1] == str[k+1] {
+			j--
+			k++
+		}
+		if k-j+1 > maxLen {
+			start = j
+			maxLen = k-j+1
+		}
+	}
+	return (string(str[start:start + maxLen]))
+}
+```
