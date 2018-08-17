4fbd01f6aae2ceb5938d70ef1bc9398105c72857
Zhu Yu <mike.zhuyu@gmail.com>
2018-08-17 15:44:17 +0800

    Update longest-common-prefix

--- leetcode/problems/longest-common-prefix.go
+++ leetcode/problems/longest-common-prefix.go
@@ -1,9 +1,5 @@
 package problems
 
-import (
-	"bytes"
-)
-
 func longestCommonPrefix(strs []string) string {
 	if len(strs) == 0 {
 		return ""
@@ -11,17 +7,10 @@ func longestCommonPrefix(strs []string) string {
 		return strs[0]
 	}
 
-	res := bytes.NewBuffer(nil)
-
 	i, stop := 0, false
-	for len(strs) > 1 {
+	for {
 		for _, s := range strs[1:] {
-			// fmt.Printf("i=%d, 0=%s, s=%s\n", i, strs[0], s)
-			if i >= len(strs[0]) || i >= len(s) {
-				stop = true
-				break
-			}
-			if strs[0][i] != s[i] {
+			if i >= len(strs[0]) || i >= len(s) || strs[0][i] != s[i] {
 				stop = true
 				break
 			}
@@ -29,9 +18,8 @@ func longestCommonPrefix(strs []string) string {
 		if stop {
 			break
 		}
-		res.WriteByte(strs[0][i])
 		i++
 	}
 
-	return res.String()
+	return strs[0][:i]
 }
