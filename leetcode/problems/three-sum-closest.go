f6dfd1bccf73be6ef0a0ca31eeb372450ff4c1e4
Zhu Yu <mike.zhuyu@gmail.com>
2018-08-27 11:22:42 +0800

    Update 3sum-closest

--- leetcode/problems/three-sum-closest.go
+++ leetcode/problems/three-sum-closest.go
@@ -1,8 +1,36 @@
 package problems
 
-import "math"
+import (
+	"fmt"
+	"math"
+	"sort"
+)
 
 func threeSumClosest(nums []int, target int) int {
+	fmt.Printf("haha: %v - %d\n", nums, target)
+	for i := 0; i < len(nums)-2; i++ {
+		nums[i] = nums[i] + nums[i+1] + nums[i+2]
+	}
+	nn := nums[0 : len(nums)-2]
+	fmt.Printf("before: %v\n", nn)
+	sort.Ints(nn)
+	fmt.Printf("after: %v\n", nn)
+	var i int
+	for i = 0; i < len(nn); i++ {
+		if nn[i]-target == 0 {
+			return nn[i]
+		} else if nn[i]-target > 0 {
+			if i > 0 && target-nn[i-1] < nn[i]-target {
+				return nn[i-1]
+			}
+			return nn[i]
+		}
+	}
+
+	return nn[len(nn)-1]
+}
+
+func threeSumClosest1(nums []int, target int) int {
 	min := math.MaxInt32
 	res := math.MinInt32
 	for i := 0; i < len(nums)-2; i++ {
--- leetcode/problems/three-sum-closest_test.go
+++ leetcode/problems/three-sum-closest_test.go
@@ -7,4 +7,16 @@ func Test_threeSumClosest(t *testing.T) {
 	if res != 2 {
 		t.Errorf("error: [-1，2，1，-4] should be 2, in fact [%d]", res)
 	}
+
+	// 新算法
+	// 1.[1,1,-1,-1,3]
+	// 3
+	// 输出：
+	// 1
+	// 预期：
+	// 3
+	res = threeSumClosest([]int{1, 1, -1, -1, 3}, 3)
+	if res != 2 {
+		t.Errorf("error: [1,1,-1,-1,3] should be 3, in fact [%d]", res)
+	}
 }
